28c271a82b6f314c6628a2cef3de570d7151c306
ppuskas <ppuskas@gmail.com>
2023-12-06 18:43:19 -0800

    cluster click animation working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { GUI } from 'dat.gui';
+import * as TWEEN from '@tweenjs/tween.js';
 
 var scene = new THREE.Scene();
 var camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000);
@@ -10,6 +11,8 @@ camera.updateProjectionMatrix(); // Update the camera's projection matrix
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+var selectedPlane = null;
 
 // Variable to control the upper end of the speed
 var speedScale = 0.01;
@@ -33,6 +36,7 @@ for (let i = 0; i < 6; i++) {
   plane.position.x = (Math.random() - 0.5) * 5;
   plane.position.y = (Math.random() - 0.5) * 5;
   plane.position.z = (Math.random() - 0.5) * 5;
+  plane.originalPosition = plane.position.clone(); // Store the original position
 
   var pivot = new THREE.Object3D();
   pivot.rotationSpeed = Math.random() * speedScale; // Random speed for each pivot, scaled by speedScale
@@ -47,15 +51,70 @@ var gui = new GUI();
 var params = { wireframe: false };
 gui.add(params, 'wireframe');
 
+// Create a raycaster and a vector to hold the mouse position
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+// Add an event listener for when the mouse is clicked
+window.addEventListener('mousedown', onMouseDown, false);
+
+function onMouseDown(event) {
+  // Convert the mouse position to normalized device coordinates (-1 to +1)
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Update the picking ray with the camera and mouse position
+  raycaster.setFromCamera(mouse, camera);
+
+  // Calculate objects intersecting the picking ray, including descendants of the scene's children
+  var intersects = raycaster.intersectObjects(scene.children, true);
+
+  if (intersects.length > 0) {
+    // If a plane was clicked, animate it moving towards the center
+    if (selectedPlane) {
+      selectedPlane.material.color.set(0x00ff00); // Change the color of the previously selected plane back to green
+      new TWEEN.Tween(selectedPlane.position)
+        .to({ x: selectedPlane.originalPosition.x, y: selectedPlane.originalPosition.y, z: selectedPlane.originalPosition.z }, 2000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .start();
+    }
+    selectedPlane = intersects[0].object;
+    selectedPlane.material.color.set(0x0000ff); // Change the color of the clicked plane to blue
+
+    // Animate the clicked plane moving towards the center
+    new TWEEN.Tween(selectedPlane.position)
+      .to({ x: 0, y: 0, z: 0 }, 2000)
+      .easing(TWEEN.Easing.Quadratic.Out)
+      .start();
+  } else {
+    // If the click was outside a plane, return to orbiting state
+    if (selectedPlane) {
+      selectedPlane.material.color.set(0x00ff00); // Change the color of the previously selected plane back to green
+      new TWEEN.Tween(selectedPlane.position)
+        .to({ x: selectedPlane.originalPosition.x, y: selectedPlane.originalPosition.y, z: selectedPlane.originalPosition.z }, 2000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .start();
+    }
+    selectedPlane = null; // Reset the selected plane
+  }
+}
+
 function animate() {
   requestAnimationFrame(animate);
 
   // Rotate each pivot point around its unique rotation axis
   pivots.forEach(pivot => {
     pivot.rotateOnAxis(pivot.rotationAxis, pivot.rotationSpeed);
+    if (pivot.children[0] === selectedPlane) {
+      pivot.children[0].material.color.set(0xff0000); // Change the color of the currently animated plane to red
+    } else {
+      pivot.children[0].material.color.set(0x00ff00); // Change the color of the other planes back to green
+    }
     pivot.children[0].lookAt(camera.position); // Make the plane face the camera
     pivot.children[0].material.wireframe = params.wireframe; // Set the wireframe mode based on the GUI toggle
   });
+
+  TWEEN.update(); // Add this line
 
   renderer.render(scene, camera);
 }
